fe45fec5211a0ae33cc500bd40a95c2e6fb125be
161905 <cristobal.carmona.14@sansano.usm.cl>
2019-09-02 13:54:08 -0400

    travis.yml up - fixed 6

--- travis.sql
+++ travis.sql
@@ -1,8 +1,6 @@
-# DB Test
-CREATE TABLE tickets(
-   id serial PRIMARY KEY,
-   title VARCHAR (50) NOT NULL,
-   description VARCHAR (355) NOT NULL,
-);
+CREATE DATABASE ticket;
+\c ticket
+
+CREATE TABLE tickets (id bigserial primary key, title VARCHAR (50), description VARCHAR (355));
 
 INSERT INTO tickets(title,description) VALUES ('test','test description');
