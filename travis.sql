21ad40f67535bcd174c2a67cbaceca1cbf00fdfe
161905 <cristobal.carmona.14@sansano.usm.cl>
2019-09-08 21:55:53 -0300

    OWASP ZAP test 2

--- travis.sql
+++ travis.sql
@@ -3,4 +3,7 @@ CREATE DATABASE ticket;
 
 CREATE TABLE tickets (id bigserial primary key, title VARCHAR (50), description VARCHAR (355));
 
-INSERT INTO tickets(title,description) VALUES ('test','test description');
+INSERT INTO tickets(title,description) VALUES ('test 1','test description 1');
+INSERT INTO tickets(title,description) VALUES ('test 2','test description 2');
+INSERT INTO tickets(title,description) VALUES ('test 3','test description 3');
+INSERT INTO tickets(title,description) VALUES ('test 4','test description 4');
